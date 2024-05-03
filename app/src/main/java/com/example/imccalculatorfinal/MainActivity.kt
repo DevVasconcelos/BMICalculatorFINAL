d94530940b3a3ae55e10ff37fd9b2c3cae54977f
DevVasconcelos <benhurarteagavasconcelos@gmail.com>
2024-05-03 17:47:41 -0300

    home screen updates color changes

---
+++ app/src/main/java/com/example/imccalculatorfinal/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.imccalculatorfinal
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Recuperar os componentes EditText
+        // Criar uma variável e associar o componente de UI<EditText
+        // Recuperar o botão da tela
+
+        // Colocar ação no botão setOnClickListener
+        // Recuperar o texto digitado no edt peso
+
+        val edtPeso = findViewById<TextInputEditText>(R.id.edt_peso)
+        val edtAltura = findViewById<TextInputEditText>(R.id.edt_altura)
+        val btnCalcular = findViewById<Button>(R.id.btn_calcular)
+
+
+        btnCalcular.setOnClickListener {
+            val peso: Float = edtPeso.text.toString().toFloat()
+            val altura: Float = edtAltura.text.toString().toFloat()
+
+            val alturaQ2 = altura * altura
+            val resultado = peso / alturaQ2
+
+            println("Vamo que vamo " + resultado)
+
+        }
+    }
+}
